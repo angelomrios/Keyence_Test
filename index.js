3484a2dde3af0e963ee2bae3ea01d08900f19c4f
Angelo Montes <angelomontesrios@gmail.com>
2023-07-03 12:15:19 -0600

    Heroku files

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use("/asistencia", usuarioRoutes);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`El servidor esta funcionando en el puerto ${port}`);
